6c2dea37858bce875165259191be7f6568a904b6
Victor Uma <umavictor11@gmail.com>
2024-07-30 12:53:20 +0100

    ToBeOrNotToBe

---
+++ Leetcode 30 days of javascript/toBeOrNotToBe/answer.ts
@@ -0,0 +1,28 @@
+type ToBeOrNotToBe = {
+  toBe: (val: any) => boolean;
+  notToBe: (val: any) => boolean;
+};
+
+function expect(val: any): ToBeOrNotToBe {
+  return {
+    toBe: (anotherVal: any): any => {
+      if (val === anotherVal) {
+        return true;
+      } else {
+        throw new Error("Not Equal");
+      }
+    },
+    notToBe: (anotherVal: any): any => {
+      if (val !== anotherVal) {
+        return true;
+      } else {
+        throw new Error("Equal");
+      }
+    },
+  };
+}
+
+/**
+ * expect(5).toBe(5); // true
+ * expect(5).notToBe(5); // throws "Equal"
+ */
