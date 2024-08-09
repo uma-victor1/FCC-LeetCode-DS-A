362c43ed92294a2b83c7a3bf9bbc7fb32b23f360
Victor Uma <umavictor11@gmail.com>
2024-08-09 11:12:24 +0100

    timeout cancellation

---
+++ Leetcode 30 days of javascript/Timeout cancellation/answer.ts
@@ -0,0 +1,47 @@
+type JSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | JSONValue[]
+  | { [key: string]: JSONValue };
+type Fn = (...args: JSONValue[]) => void;
+
+function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+  let time = setTimeout(() => fn(...args), t);
+  return () => {
+    clearTimeout(time);
+  };
+}
+
+/**
+  my notes
+ time to run function is given
+ you can't run function if it takes fn() longer than cancellation time
+
+
+ */
+
+/**
+ *  const result = [];
+ *
+ *  const fn = (x) => x * 5;
+ *  const args = [2], t = 20, cancelTimeMs = 50;
+ *
+ *  const start = performance.now();
+ *
+ *  const log = (...argsArr) => {
+ *      const diff = Math.floor(performance.now() - start);
+ *      result.push({"time": diff, "returned": fn(...argsArr)});
+ *  }
+ *
+ *  const cancel = cancellable(log, args, t);
+ *
+ *  const maxT = Math.max(t, cancelTimeMs);
+ *
+ *  setTimeout(cancel, cancelTimeMs);
+ *
+ *  setTimeout(() => {
+ *      console.log(result); // [{"time":20,"returned":10}]
+ *  }, maxT + 15)
+ */
