3d438306a675437a1684552f12ac16462988b176
Victor Uma <umavictor11@gmail.com>
2024-08-10 21:48:28 +0100

    timeout

--- Leetcode 30 days of javascript/Timeout cancellation/answer.ts
+++ Leetcode 30 days of javascript/Timeout cancellation/answer.ts
@@ -7,7 +7,7 @@ type JSONValue =
   | { [key: string]: JSONValue };
 type Fn = (...args: JSONValue[]) => void;
 
-function cancellable(fn: Fn, args: JSONValue[], t: number): Function {
+function timeoutCancellable(fn: Fn, args: JSONValue[], t: number): Function {
   let time = setTimeout(() => fn(...args), t);
   return () => {
     clearTimeout(time);
