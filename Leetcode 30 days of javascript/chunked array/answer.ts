8a4c1c0ad34a792588e3c904eda6d566b52ea4ef
Victor Uma <umavictor11@gmail.com>
2024-08-21 23:48:45 +0100

    chunked array

---
+++ Leetcode 30 days of javascript/chunked array/answer.ts
@@ -0,0 +1,21 @@
+type ChunkedJSONValue =
+  | null
+  | boolean
+  | number
+  | string
+  | ChunkedJSONValue[]
+  | { [key: string]: ChunkedJSONValue };
+type Obj = Record<string, ChunkedJSONValue> | Array<ChunkedJSONValue>;
+
+// given an array [] and size
+// return [[],[]] chunked array of size.
+
+function chunk(arr: Obj[], size: number): Obj[][] {
+  let newChunkedArr = [];
+
+  for (let i = 0; i < arr.length; i += size) {
+    let cut = i + size;
+    newChunkedArr.push(arr.slice(i, cut));
+  }
+  return newChunkedArr;
+}
