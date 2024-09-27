29972b92ad9c330f66021151a735fa2f1d48172d
Victor Uma <umavictor11@gmail.com>
2024-09-27 13:27:49 +0100

    linkedlist

---
+++ linkedlist.js
@@ -0,0 +1,32 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  append(data) {
+    const newNode = new Node(data);
+    if (!this.head) {
+      this.head = newNode;
+    } else {
+      let current = this.head;
+      while (current.next !== null) {
+        current = current.next;
+      }
+      current.next = newNode;
+    }
+  }
+}
+
+let linkedlist = new LinkedList();
+linkedlist.append(10);
+linkedlist.append(20);
+linkedlist.append(30);
+
+console.log(linkedlist);
